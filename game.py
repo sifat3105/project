db4a079c978e48896393e20652c8234b62d388c7
MD SIFAT ALI <sifatali520@gmail.com>
2024-04-09 23:27:56 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,16 +1,35 @@
 import random
-user_choice = input("Enter Your Choice: R for Rock, P for Paper, and S for Scissors")
-computer_choice = random.randint(0,2)
+while True:
+    user_choice = input("Enter Your Choice: Rock, Paper, and Scissors :")
+    computer_choice = random.randint(0,2)
+    computer_choice_list = ['Rock', 'Paper', 'Scissors']
+    print(f"Computer Choice : {computer_choice_list[computer_choice]}")
+    if user_choice[0] == 'R' or user_choice[0] == 'r':
+        user_choice = 0
+    elif user_choice[0] == 'P' or user_choice[0] == 'p':
+        user_choice = 1
+    elif user_choice[0] == 'S' or user_choice[0] == 's':
+        user_choice = 2
+    elif user_choice == 'exit':
+        print("Exiting the game.")
+        break
+    else:
+        print("your Choice is Wrong")
+        continue    
 
-if user_choice == computer_choice:
-    print("It's a Diow")
-elif user_choice == 0 and computer_choice == 2:
-    print("You Win")
-elif user_choice == 2 and computer_choice == 0:
-    print("You lose")
-elif user_choice < computer_choice:
-    print("you lose")
-elif user_choice > computer_choice:
-    print("you win")
-else:
-    print("Please Enter Your Choice in 'R' 'P' 'S'")
+    if user_choice == computer_choice:
+        print("It's a Diow")
+    elif user_choice == 0 and computer_choice == 2:
+        print("You Win")
+    elif user_choice == 2 and computer_choice == 0:
+        print("You lose")
+    elif user_choice < computer_choice:
+        print("you lose")
+    elif user_choice > computer_choice:
+        print("you win")
+    
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again == 'no':
+        print("Exiting the game.")
+        break
+    
